76c4dc29d6939f5659ab80a5105dc7af863e23f7
hcuk94 <me@hc.me.uk>
2021-07-12 10:20:00 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,139 @@
+<html>
+<head>
+<title>Henry Cole</title>
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link href="style-local.css" rel="stylesheet">
+</head>
+<body>
+<!-- Begin 'Sidebar' Section -->
+<div class="container">
+<div class="row">&nbsp;</div>
+<div class="row">
+<div class="col-xl-3">
+<img src="henry-profile.jpg" width="250px" class="img-thumbnail rounded img-centre" alt="Henry Cole Profile Picture"/>
+<h1 class="text-center display-5 mt-2">Henry Cole</h1>
+<p class="text-center lead mt-2">Systems Analyst with 10 years' experience in enterprise IT</p>
+<span class="text-center img-centre">
+<nav class="nav justify-content-center">
+<a class="nav-link text-secondary" href="#">twitter</a>
+<a class="nav-link text-secondary" href="#">discord</a>
+<a class="nav-link text-secondary" href="#">github</a>
+</nav>
+</span>
+<br/>
+</div>
+<!-- End 'Sidebar' Section -->
+<!-- Main Column -->
+<div class="col-lg">
+<!-- Welcome -->
+<h2>Hello</h2>
+<p>My name is Henry Cole. I'm a systems analyst from London. I currently work at PRS for Music, primarily with Oracle/Java applications. 
+In my spare time I like travelling, visiting quirky pubs, and tinkering with personal code/computer projects.</p>
+<div class="row mt-4">
+    <div class="col-sm-auto">
+        <h6>Technical Skills:</h6>
+        <ul class="list-unstyled">
+            <li>Oracle Database</li>
+            <li>Apache Tomcat</li>
+            <li>Office 365</li>
+            <li>Azure Active Directory</li>
+            <li>TCP/IP Networking</li>
+            <li>WiFi</li>
+            <li>SIP/VOIP Telephony</li>
+            <li>Access Control & CCTV Systems</li>
+        </ul>
+    </div>
+    <div class="col-sm-auto">
+        <h6>Code:</h6>
+        <ul class="list-unstyled">
+            <li>SQL (Oracle, Microsoft & MySQL)</li>
+            <li>Python</li>
+            <li>HTML5/CSS</li>
+            <li>Java</li>
+        </ul>
+    </div>
+    <div class="col-sm-auto">
+        <h6>Other Skills:</h6>
+        <ul class="list-unstyled">
+            <li>Application Portfolio Management</li>
+            <li>IT Service Management</li>
+            <li>English</li>
+            <li>French</li>
+            <li>German</li>
+        </ul>
+    </div>
+</div>
+
+<!-- Work History -->
+<h2 class="mt-4">Work</h2>
+<div class="row d-flex">
+<div class="col-sm-auto mb-3">
+<div class="card">
+  <img src="/img/client/prs-for-music.jpg" class="card-img-top pt-2 img-centre" alt="PRS for Music Logo">
+  <div class="card-body">
+    <h5 class="card-title">PRS for Music</h5>
+    <h5><small class="text-muted">2019-present</small></h5>
+    <p class="card-text">PRS for Music represents the rights of over 150,000 songwriters & composers.</p>
+    <p class="card-text">I work on supporting and maintaining internal Oracle/Java based applications</p>
+    <span class="badge bg-light text-dark">Oracle Database</span>
+    <span class="badge bg-light text-dark">PL/SQL</span>
+    <span class="badge bg-light text-dark">Java</span>
+    <span class="badge bg-light text-dark">Apache Tomcat</span>
+    <span class="badge bg-light text-dark">Microsoft SQL Server</span>
+    <span class="badge bg-light text-dark">C#</span>
+    <span class="badge bg-light text-dark">IT Service Management</span>
+    <span class="badge bg-light text-dark">Audit</span>
+    <span class="badge bg-light text-dark">Systems Documentation</span>
+  </div>
+</div></div>
+<div class="col-sm-auto mb-3">
+<div class="card">
+  <img src="/img/client/abellio.jpg" class="card-img-top pt-2 img-centre" alt="Abellio Logo">
+  <div class="card-body">
+    <h5 class="card-title">Abellio</h5>
+    <h5><small class="text-muted">2012-2019</small></h5>
+    <p class="card-text">Abellio is a public transport operating company which runs rail and bus contracts across the UK and Germany.</p>
+    <p class="card-text">I worked for Abellio for 7 years, mostly doing some things.</p>
+    <span class="badge bg-light text-dark">TCP/IP Networking</span>
+    <span class="badge bg-light text-dark">Active Directory</span>
+    <span class="badge bg-light text-dark">Office 365</span>
+    <span class="badge bg-light text-dark">Azure</span>
+    <span class="badge bg-light text-dark">On-Train WiFi</span>
+    <span class="badge bg-light text-dark">Datacentre Migration</span>
+    <span class="badge bg-light text-dark">Citrix</span>
+    <span class="badge bg-light text-dark">SIP/VOIP Telephony</span>
+    <span class="badge bg-light text-dark">Access Control</span>
+    <span class="badge bg-light text-dark">CCTV Systems</span>
+  </div>
+</div></div>
+<div class="col-sm-auto mb-3">
+<div class="card">
+  <img src="/img/client/the-windmill.jpg" class="card-img-top pt-2 img-centre" alt="The Windmill Chatham Green Logo">
+  <div class="card-body">
+    <h5 class="card-title">The Windmill</h5>
+    <h5><small class="text-muted">ad-hoc</small></h5>
+    <p class="card-text">The Windmill is a Michelin award-winning pub restaurant in Essex.</p>
+    <p class="card-text">I operate and maintain the website, as well as running the network & infrastructure e.g. CCTV on-site.</p>
+    <span class="badge bg-light text-dark">TCP/IP Networking</span>
+    <span class="badge bg-light text-dark">WiFi</span>
+    <span class="badge bg-light text-dark">CCTV Systems</span>
+    <span class="badge bg-light text-dark">Wordpress</span>
+    <span class="badge bg-light text-dark">PHP</span>
+    <span class="badge bg-light text-dark">MySQL</span>
+    <span class="badge bg-light text-dark">SIP/VOIP Telephony</span>
+  </div>
+</div></div>
+</div>
+<!-- Blog -->
+<h2 class="mt-4">Blog</h2>
+<p>Maybe one day I'll have one...</p>
+<!-- Contact -->
+<h2 class="mt-4">Contact Me</h2>
+<p>You can reach me by email, Twitter, Discord. If you're lucky I might tell you how.</p>
+</div>
+</div>
+<div class="row">&nbsp;</div>
+</div>
+</body>
+</html>
